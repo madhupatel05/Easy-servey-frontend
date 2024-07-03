3a390d842d6e36d3bf636ff038374ad9251d4d93
madhu <madhugurjar9098@gmail.com>
2024-07-03 16:44:11 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/Home/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of your React application.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
